5b3f4875ba9a353db651825625cd356ad33488a2
Mason Beale <mbeale@cedarville.edu>
2022-01-13 20:42:36 -0500

    intentionaly made test fail

--- src/test/java/com/masonbeale/MainTest.java
+++ src/test/java/com/masonbeale/MainTest.java
@@ -3,6 +3,8 @@ package com.masonbeale;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanClassTest {
@@ -13,16 +15,20 @@ class HangmanClassTest {
         boolean loss = hangman.CheckWinLoss(-1, 0, "Test");
         boolean win = hangman.CheckWinLoss(4, 5, "Test");
 
-        assertFalse(loss);
+        assertTrue(loss);
         assertFalse(win);
     }
-    @Test
+    // need to figure out how to test function that needs user input
+    /*@Test
     @DisplayName("Check guess name method")
     void checkGuessName(){
         Hangman hangman = new Hangman();
         // currently, word argument is like main function and not dynamic
         boolean correctWord = hangman.GuessWord("moose");
-        assertTrue(correctWord);
-    }
+        Scanner scanner = new Scanner(System.in);
+        String answer = "y";
+        System.in(answer);
+        assertFalse(correctWord);
+    }*/
 
 }
